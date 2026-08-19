491ff5bb364e6b9b48e15b6b19176347c19a700c
Abbas Abdulraheem Bello <aabsmw404@gmail.com>
2026-08-19 20:03:24 +0100

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,7 @@ import { AppModule } from './app.module.js';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:9000',
-      'http://localhost:5173',
-    ],
+    origin: ['https://tin-tin-six.vercel.app/', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
